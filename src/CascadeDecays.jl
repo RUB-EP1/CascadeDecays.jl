9d37fd4afaec751fc365ed3a995fedfe0d196348
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2026-06-04 16:04:49 +0200

    clean up include/export blocks

--- src/CascadeDecays.jl
+++ src/CascadeDecays.jl
@@ -7,18 +7,17 @@ using InstructionalDecayTrees
 import ThreeBodyDecays
 import ThreeBodyDecays: SpinParity, VertexFunction, RecouplingLS, ⊗
 
+# interfaces
 export AbstractLineshape, AbstractVertex
-export ConstantLineshape, PropagatorFunction
+export ConstantLineshape
+include("interfaces.jl")
 
-export SystemMasses, SystemSpins, SystemParities, SystemSpinParities
-export CascadeSystemWithParities
-export UndefinedParity
-export final_two_js, root_two_j, final_masses, root_mass
-# 
-export CascadeSystem, CascadeKinematics
-export SpinParity, VertexFunction
-export DecayTopology, DecayChain
-# topology methods
+# propagators
+export PropagatorFunction
+include("propagator_function.jl")
+
+# topology
+export DecayTopology
 export relation, rootline, finallines, nlines, nvertices, nfinal
 export incoming_lines, outgoing_lines, incoming_line
 export child_lines, vertex_lines, final_descendants
@@ -26,30 +25,47 @@ export produced_by, consumed_by
 export internal_lines, propagating_lines
 export isrootline, isfinalline, isinternalline
 export has_canonical_line_order
-# 
-export line_masses2
-export line_values, line_two_js
-export vertex_masses2, vertex_helicities, vertex_spins, vertex_angles
+export bracket, validate_topology
+include("topology.jl")
+
+# quantum numbers
+export SystemMasses, SystemSpins, SystemParities, SystemSpinParities
+export UndefinedParity
+export final_two_js, root_two_j
+include("quantum_numbers.jl")
+
+# possible_ls_more (internal; used by ls_coupling)
+include("possible_ls_more.jl")
+
+# system
+export CascadeSystem, CascadeSystemWithParities
+export final_masses, root_mass
+export line_masses2, line_values
+include("system.jl")
+
+# kinematics
+export CascadeKinematics
 export line_invariant
-# 
+export vertex_masses2, vertex_helicities, vertex_spins, vertex_angles
+include("kinematics.jl")
+
+# decay chain
+export DecayChain
+export SpinParity, VertexFunction
+export line_two_js
+include("chain.jl")
+
+# evaluation
 export HelicityRootFrame, CurrentFrame
 export helicity_angle_program, helicity_angle_programs, cascade_kinematics
 export routed_vertex_amplitude, routed_propagator_product, amplitude
-export bracket, validate_topology
+include("evaluation.jl")
+
+# LS coupling
 export possible_vertex_ls, minimal_vertex_coupling
 export possible_vertex_couplings, minimal_vertex_couplings
 export line_spin_parities, vertex_spin_parities
 export minimal_ls_decay_chain, all_ls_decay_chains
-
-include("interfaces.jl")
-include("propagator_function.jl")
-include("topology.jl")
-include("quantum_numbers.jl")
-include("possible_ls_more.jl")
-include("system.jl")
-include("kinematics.jl")
-include("chain.jl")
-include("evaluation.jl")
 include("ls_coupling.jl")
 
 end
